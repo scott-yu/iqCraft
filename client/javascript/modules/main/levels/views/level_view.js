be476f2a1e7c34e494923dd6892daee213ff604d
Scott Yu <scottt@iqmetrix.com>
2015-02-24 10:05:34 -0800

    new methods to do something

--- client/javascript/modules/main/levels/views/level_view.js
+++ client/javascript/modules/main/levels/views/level_view.js
@@ -1,6 +1,18 @@
 var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.ItemView.extend({
-  tagName: 'li',
-  template: require('./templates/level_template')
+    tagName: 'li',
+    template: require('./templates/level_template'),
+    initialize: function() {
+        this._doSomething();
+        this._doSomethingMaybe();
+    },
+    _doSomething: function() {
+        return;
+    },
+    _doSomethingMaybe: function() {
+        return Math.random() === 0;
+
+        return 'foooooooooo';
+    }
 });
